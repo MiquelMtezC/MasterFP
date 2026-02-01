6f9924485687fa70104a3925194b9636ebc38bb3
isard <isard@debian>
2026-02-01 15:50:40 +0100

    tests

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Question, Choice
 
-# Create your tests here.
+class PollsModelsTest(TestCase):
+    def test_creacio_question(self):
+        # Creem una pregunta de prova
+        q = Question.objects.create(question_text="Pregunta de prova?")
+        self.assertEqual(q.question_text, "Pregunta de prova?")
+
+    def test_creacio_choice(self):
+        q = Question.objects.create(question_text="Pregunta 2?")
+        c = Choice.objects.create(question=q, choice_text="Opció 1", votes=0)
+        self.assertEqual(c.choice_text, "Opció 1")
+        self.assertEqual(c.votes, 0)
